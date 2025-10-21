6010ff5db7c8c81671304b3aa5dc71b53cd4dc1d
ToAsh23 <165156220+ToAsh23@users.noreply.github.com>
2025-10-21 09:37:08 +0200

    Introduce number and square data in app.py
    Add data processing for numbers and their squares

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title("Bonjour à tous !")
 
 st.write('Mineure numérique')
+
+nombre = [1,2,3,4]
+carre = [1**1,2**2,3**3,4**4]
+
+d = {"nombre :" nombre, "carré:" carre}
+
+data = pd.DataFrame (d)
