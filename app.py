628770f456918279cd28a19a302fa1a268592dde
ToAsh23 <165156220+ToAsh23@users.noreply.github.com>
2025-10-21 09:52:59 +0200

    Enhance DataFrame display with Streamlit headers
    Added headers and descriptions for DataFrames in Streamlit.

--- app.py
+++ app.py
@@ -10,5 +10,16 @@ carre = [1**1,2**2,3**3,4**4]
 d = {"nombre" : nombre, "carré": carre}
 
 data = pd.DataFrame (d)
+st.header("Premier exemple")
+st.write("Voici un DataFrame de nombre et leur carré par elle même :")
+st.dataframe(data)
 
-print (data)
+# Créer un exemple de DataFrame
+df = pd.DataFrame({
+    'colonne 1': [1, 2, 3, 4],
+    'colonne 2': [10, 20, 30, 40]
+})
+
+st.header("Affichage de Données")
+st.write("Voici un DataFrame :")
+st.dataframe(df) # Affiche une table interactive
