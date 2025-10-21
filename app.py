206a7cfa6b788eb625fe1cd967cf414bbfeb4e53
ToAsh23 <165156220+ToAsh23@users.noreply.github.com>
2025-10-21 09:41:19 +0200

    Fix dictionary syntax in app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.write('Mineure numérique')
 nombre = [1,2,3,4]
 carre = [1**1,2**2,3**3,4**4]
 
-d = {"nombre :" nombre, "carré:" carre}
+d = {"nombre" : nombre, "carré": carre}
 
 data = pd.DataFrame (d)
